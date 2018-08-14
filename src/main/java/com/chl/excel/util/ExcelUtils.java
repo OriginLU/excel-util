5b6e45672a79211b14501ccfe065d2bfc20e065a
LCH <chl_it@126.com>
2018-08-14 09:51:34 +0800

    excel导出工具

--- src/main/java/com/chl/excel/util/ExcelUtils.java
+++ src/main/java/com/chl/excel/util/ExcelUtils.java
@@ -20,12 +20,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 /**
+ * because of poi as the tool of excel for operation is not be safe in the multi thread,
+ * you can overwrite the {@link Row#createCell(int)} and {@link Sheet#createRow(int)} method
+ * and add lock for guarantee safe operation,but it performance bad than single thread
  * @author LCH
  * @since 2018-06-13
  */
@@ -36,82 +38,20 @@ public abstract class ExcelUtils {
 
     private static int CELL_WIDTH = 20;
 
-    private static int SHEET_COUNT = 500;
-
-    private static Sequence sequence = new Sequence(1l, 1l);
+    private static int SHEET_COUNT = 10000;
 
 
+    private static Object getResult(ExcelColumnConf config,Object obj) throws InvocationTargetException {
 
-    public static Workbook createExcelAdvance(List list, Class type){
-
-        if (type.getAnnotation(Excel.class) == null) {
-
+        Field field = config.getAnnotationField();
+        if (field != null){
+            return ReflectUtils.getFieldValue(obj, field);
         }
-        int length = list.size();
-        String titleName = ExcelConfigureUtil.getExcelTitleName(type);
-        String excelVersion = ExcelConfigureUtil.getExcelVersion(type);
-        ExcelColumnConf[] conf = ExcelConfigureUtil.getExcelColumnConfiguration(type);
-        Workbook workbook = WorkBookFactory.createWorkBook(excelVersion);
-
-        Sheet sheet = workbook.createSheet(titleName);
-        sheet.setDefaultColumnWidth(CELL_WIDTH);
-
-        int rowNum = createTitleRow(workbook,sheet,titleName,conf.length,true);
-        rowNum = createColumnName(workbook,sheet,conf,rowNum,true);
-
-        int cycleCount = getCycleCount(length);
-        CountDownLatch latch = new CountDownLatch(cycleCount);
-
-        for (int i = rowNum , j = 0; j < cycleCount; i++ , j++) {
-            int start = getStartIndex(cycleCount,i);
-            int end = getEndIndex(cycleCount,length,i);
-            createRowTask(sheet, list.get(j),conf,latch,start,end);
+        Method method = config.getAnnotationMethod();
+        if (method != null){
+            return ReflectUtils.invokeMethod(obj, method);
         }
-        return getWorkBook(workbook,latch);
-    }
-
-    private static int getEndIndex(int cycleCount, int length, int i) {
-        int end = cycleCount * i + SHEET_COUNT;
-        return length <= end?  length : end;
-    }
-
-    private static int getStartIndex(int cycleCount, int i) {
-        return i*cycleCount;
-    }
-
-
-    private static Workbook getWorkBook(Workbook workbook, CountDownLatch latch) {
-
-        try {
-            latch.await();
-            return workbook;
-        }catch (InterruptedException e){
-            throw new ExcelCreateException("",e);
-        }
-    }
-
-
-    private static void createRowTask(final Sheet sheet, final Object obj, final ExcelColumnConf[] configs,final CountDownLatch latch,final int start,final int end){
-
-        ExecutorService executorService = ExecutorFactory.getInstance();
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    for (int i = start; i < end; i++) {
-                        Row row = getRow(sheet, i);
-                        createDataRow(row,obj,configs);
-                    }
-                }finally {
-                    latch.countDown();
-                }
-            }
-        });
-    }
-
-    public static synchronized Row getRow(Sheet sheet,int index){
-        Row row = sheet.createRow(index);
-        return row;
+        return null;
     }
 
     public static Workbook createExcel(List list, Class type) {
@@ -201,6 +141,87 @@ public abstract class ExcelUtils {
     }
 
 
+    public static void close() {
+        ExecutorFactory.close();
+    }
+
+
+    private static int getCycleCount(int size) {
+
+        int cycleCount = 0;
+        if ((size % SHEET_COUNT) != 0) {
+            return (size / SHEET_COUNT) + 1;
+        }
+        return (size / SHEET_COUNT);
+    }
+
+
+    /**
+     * 设置单元格样式
+     *
+     * @param wb
+     * @return
+     */
+    private static CellStyle getContentCellStyle(Workbook wb) {
+
+        Font cellFont = wb.createFont();
+        cellFont.setItalic(false);                                      // 设置字体为斜体字
+        cellFont.setFontName("宋体");                                    // 字体应用到当前单元格上
+        cellFont.setColor(Font.COLOR_NORMAL);                           // 将字体设置为“红色”
+        cellFont.setFontHeightInPoints((short) 10);                      // 将字体大小设置为18px
+        cellFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+
+        CellStyle cellStyle = wb.createCellStyle();                     //表格样式
+        cellStyle.setFont(cellFont);
+        cellStyle.setWrapText(true);                                    //设置自动换行
+        cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        cellStyle.setBorderTop(HSSFCellStyle.BORDER_THIN);              //上边框
+        cellStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN);             //左边框
+        cellStyle.setBorderRight(HSSFCellStyle.BORDER_THIN);            //右边框
+        cellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);           //下边框
+        cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+
+        return cellStyle;
+    }
+
+    private static void createDataRow(Row row, Object obj, ExcelColumnConf[] configs) {
+
+        try {
+            for (int i = 0, length = configs.length; i < length; i++) {
+                ExcelColumnConf config = configs[i];
+                Object result = getResult(config, obj);
+                row.createCell(i).setCellValue(convertToString(result, config.getAnnotations()));
+            }
+        } catch (InvocationTargetException e) {
+            log.error("invoke method throw exception : ", e);
+            throw new ExcelCreateException("invoke method error ", e);
+        }
+    }
+
+    private static void setCell(Row row, Object result, ExcelColumnConf config, int index) {
+        row.createCell(index).setCellValue(convertToString(result, config.getAnnotations()));
+    }
+
+    private static String convertToString(Object result, Map<Class, Annotation> ans) {
+        if (result == null) {
+            return "";
+        }
+        return result.toString();
+    }
+
+
+    private static CellStyle getCellStyle(SXSSFWorkbook workbook) {
+
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+        Font font = workbook.createFont();
+        font.setBoldweight(Font.BOLDWEIGHT_NORMAL);
+        cellStyle.setFont(font);
+
+        return cellStyle;
+    }
+
     /**
      * create executor service by ExecutorFactory
      */
@@ -249,95 +270,5 @@ public abstract class ExcelUtils {
                 return t;
             }
         }
-    }
-
-
-    public static void close() {
-        ExecutorFactory.close();
-    }
-
-
-    private static int getCycleCount(int size) {
-
-        int cycleCount = 0;
-        if ((size % SHEET_COUNT) != 0) {
-            return (size / SHEET_COUNT) + 1;
-        }
-        return (size / SHEET_COUNT);
-    }
-
-
-    /**
-     * 设置单元格样式
-     *
-     * @param wb
-     * @return
-     */
-    private static CellStyle getContentCellStyle(Workbook wb) {
-
-        Font cellFont = wb.createFont();
-        cellFont.setItalic(false);                                      // 设置字体为斜体字
-        cellFont.setFontName("宋体");                                    // 字体应用到当前单元格上
-        cellFont.setColor(Font.COLOR_NORMAL);                           // 将字体设置为“红色”
-        cellFont.setFontHeightInPoints((short) 10);                      // 将字体大小设置为18px
-        cellFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-
-        CellStyle cellStyle = wb.createCellStyle();                     //表格样式
-        cellStyle.setFont(cellFont);
-        cellStyle.setWrapText(true);                                    //设置自动换行
-        cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-        cellStyle.setBorderTop(HSSFCellStyle.BORDER_THIN);              //上边框
-        cellStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN);             //左边框
-        cellStyle.setBorderRight(HSSFCellStyle.BORDER_THIN);            //右边框
-        cellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN);           //下边框
-        cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-
-        return cellStyle;
-    }
-
-    private static void createDataRow(Row row, Object obj, ExcelColumnConf[] configs) {
-
-        try {
-            Object result = null;
-            for (int i = 0, length = configs.length; i < length; i++) {
-                ExcelColumnConf config = configs[i];
-                Field field = config.getAnnotationField();
-                Method method = config.getAnnotationMethod();
-                if (field != null) {
-                    result = ReflectUtils.getFieldValue(obj, field);
-                }
-                if (method != null) {
-                    result = ReflectUtils.invokeMethod(obj, method);
-                }
-                setCell(row,result,config,i);
-            }
-        } catch (InvocationTargetException e) {
-            log.error("invoke method throw exception : ", e);
-            throw new ExcelCreateException("invoke method error ", e);
-        }
-    }
-
-    private static synchronized void setCell(Row row,Object result,ExcelColumnConf config,int index){
-        row.createCell(index).setCellValue(convertToString(result, config.getAnnotations()));
-    }
-
-    private static String convertToString(Object result, Map<Class, Annotation> ans) {
-        if (result == null) {
-            return "";
-        }
-        return result.toString();
-    }
-
-
-    private static CellStyle getCellStyle(SXSSFWorkbook workbook) {
-
-
-        CellStyle cellStyle = workbook.createCellStyle();
-        cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
-        Font font = workbook.createFont();
-        font.setBoldweight(Font.BOLDWEIGHT_NORMAL);
-        cellStyle.setFont(font);
-
-        return cellStyle;
     }
 }
