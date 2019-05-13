beb19bd4ea3fbf8595e0482a3288990f3c548479
chl <0016005176@nubia.com>
2019-05-13 14:27:01 +0800

    修改路径获取方法

--- src/main/java/com/myframe/excel/util/FileUtils.java
+++ src/main/java/com/myframe/excel/util/FileUtils.java
@@ -11,17 +11,16 @@ public  class FileUtils {
 
         File file = new File(path);
 
-        if (!file.isDirectory())
-        {
-            throw new IllegalArgumentException("the required parameter is file path,the input parameter is [" + path + "], check please");
-        }
-
         if (!file.exists())
         {
             if (!file.mkdir())
             {
-                throw new IllegalStateException("can't create file path [" + path + "]");
+                throw new IllegalStateException("can't create file path [" + path + "],please check if the permissions are sufficient");
             }
+        }
+        else if (file.isFile())
+        {
+            throw new IllegalArgumentException("the required parameter is file path,the input parameter is [" + path + "], check please");
         }
 
         return file.getAbsolutePath() + File.separator;
